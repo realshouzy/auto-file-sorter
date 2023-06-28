0a74699b56e48844147809ae6145229c6f6e6dd3
shouzy <shouzy@tuta.io>
2023-06-28 18:04:12 +0200

    Moved __status__ under __version__

--- src/auto_file_sorter/__init__.py
+++ src/auto_file_sorter/__init__.py
@@ -4,10 +4,10 @@
 from __future__ import annotations
 
 __version__: Final[str] = "2.0.0"
+__status__: Final[str] = "development"
 __title__: Final[str] = "auto-file-sorter"
 __author__: Final[str] = "realshouzy"
 __license__: Final[str] = "MIT"
 __copyright__: Final[str] = "Copyright (c) 2022-present realshouzy"
-__status__: Final[str] = "development"
 
 from typing import Final
