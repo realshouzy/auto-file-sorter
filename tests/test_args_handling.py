3abee941599890c6290c60b1afd486d43084d3b2
shouzy <82171453+realshouzy@users.noreply.github.com>
2023-07-23 22:50:50 +0200

    Added more tests

---
+++ tests/test_args_handling.py
@@ -0,0 +1,94 @@
+"""Module testing ``auto_file_sorter.args_handling.py``."""
+
+from __future__ import annotations
+
+import argparse
+from typing import TYPE_CHECKING
+
+import pytest
+
+# pylint: disable=C0116, W0611
+from auto_file_sorter.args_handling import handle_locations_args
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+@pytest.mark.skip(reason="Test needs to be written")  # type: ignore[misc]
+def test_handle_write_args() -> None:
+    ...
+
+
+@pytest.mark.skip(reason="Test needs to be written")  # type: ignore[misc]
+def test_handle_read_args() -> None:
+    ...
+
+
+@pytest.mark.skip(reason="Test needs to be written")  # type: ignore[misc]
+def test_handle_track_args() -> None:
+    ...
+
+
+def test_handle_locations_args_get_log_location(
+    tmp_path: Path,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    test_log_file: Path = tmp_path.joinpath("auto-file-sorter.log")
+
+    args = argparse.Namespace(
+        log_location=test_log_file,
+        get_log_location=True,
+        get_config_location=False,
+    )
+
+    exit_code: int = handle_locations_args(args)
+
+    out, _ = capsys.readouterr()
+
+    assert out == f"{test_log_file}\n"
+
+    assert exit_code == 0
+
+
+def test_handle_locations_args_get_configs_location(
+    tmp_path: Path,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    test_configs_file: Path = tmp_path.joinpath("configs.json")
+
+    args = argparse.Namespace(
+        configs_location=test_configs_file,
+        get_log_location=False,
+        get_config_location=True,
+    )
+
+    exit_code: int = handle_locations_args(args)
+
+    out, _ = capsys.readouterr()
+
+    assert out == f"{test_configs_file}\n"
+
+    assert exit_code == 0
+
+
+def test_handle_locations_args_both(
+    tmp_path: Path,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    test_log_file: Path = tmp_path.joinpath("auto-file-sorter.log")
+    test_configs_file: Path = tmp_path.joinpath("configs.json")
+
+    args = argparse.Namespace(
+        log_location=test_log_file,
+        configs_location=test_configs_file,
+        get_log_location=True,
+        get_config_location=True,
+    )
+
+    exit_code: int = handle_locations_args(args)
+
+    out, _ = capsys.readouterr()
+
+    assert out == f"{test_log_file}\n{test_configs_file}\n"
+
+    assert exit_code == 0
---
+++ tests/test_event_handling.py
@@ -0,0 +1,148 @@
+"""Module testing ``auto_file_sorter.event_handling.py``."""
+from __future__ import annotations
+
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+import pytest
+from watchdog.events import FileSystemEventHandler
+
+from auto_file_sorter.event_handling import OnModifiedEventHandler
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+# pylint: disable=C0116, W0212, W0621
+
+
+@pytest.fixture()  # type: ignore[misc]
+def extension_paths(tmp_path: Path) -> dict[str, Path]:
+    return {
+        ".txt": tmp_path / "txt",
+        ".doc": tmp_path / "doc",
+    }
+
+
+@pytest.fixture()  # type: ignore[misc]
+def path_for_undefined_extensions(tmp_path: Path) -> Path:
+    return tmp_path.joinpath("undefined")
+
+
+def test_on_modified_event_handler_base() -> None:
+    assert OnModifiedEventHandler.__base__ == FileSystemEventHandler
+
+
+def test_on_modified_event_handler_str(
+    tmp_path: Path,
+    extension_paths: dict[str, Path],
+    path_for_undefined_extensions: Path,
+) -> None:
+    test_event_handler: OnModifiedEventHandler = OnModifiedEventHandler(
+        tmp_path,
+        extension_paths,
+        path_for_undefined_extensions=path_for_undefined_extensions,
+    )
+    assert str(test_event_handler) == f"OnModifiedEventHandler('{tmp_path}')"
+
+
+def test_on_modified_event_handler_str_no_path_for_undefined_extensions(
+    tmp_path: Path,
+    extension_paths: dict[str, Path],
+) -> None:
+    test_event_handler: OnModifiedEventHandler = OnModifiedEventHandler(
+        tmp_path,
+        extension_paths,
+        path_for_undefined_extensions=None,
+    )
+    assert str(test_event_handler) == f"OnModifiedEventHandler('{tmp_path}')"
+
+
+def test_on_modified_event_handler_repr(
+    tmp_path: Path,
+    extension_paths: dict[str, Path],
+    path_for_undefined_extensions: Path,
+) -> None:
+    test_event_handler: OnModifiedEventHandler = OnModifiedEventHandler(
+        tmp_path,
+        extension_paths,
+        path_for_undefined_extensions=path_for_undefined_extensions,
+    )
+    repr_of_test_event_handler: str = (
+        f"OnModifiedEventHandler(tracked_path={tmp_path!r}, "
+        f"extension_paths={extension_paths!r}, "
+        f"path_for_undefined_extensions={path_for_undefined_extensions!r})"
+    )
+    assert repr(test_event_handler) == repr_of_test_event_handler
+
+
+def test_on_modified_event_handler_repr_no_path_for_undefined_extensions(
+    tmp_path: Path,
+    extension_paths: dict[str, Path],
+) -> None:
+    test_event_handler: OnModifiedEventHandler = OnModifiedEventHandler(
+        tmp_path,
+        extension_paths,
+        path_for_undefined_extensions=None,
+    )
+    repr_of_test_event_handler: str = (
+        f"OnModifiedEventHandler(tracked_path={tmp_path!r}, "
+        f"extension_paths={extension_paths!r}, "
+        f"path_for_undefined_extensions={None!r})"
+    )
+    assert repr(test_event_handler) == repr_of_test_event_handler
+
+
+def test_on_modified_event_handler_add_date_to_path(tmp_path: Path) -> None:
+    event_handler: OnModifiedEventHandler = OnModifiedEventHandler(tmp_path, {}, None)
+
+    dated_path: Path = event_handler._add_date_to_path(tmp_path)  # type: ignore[unused-ignore]
+
+    assert dated_path.exists()
+    assert dated_path == tmp_path / f"{datetime.now():%Y/%b}"
+
+
+def test_on_modified_event_handler_increment_file_name(tmp_path: Path) -> None:
+    event_handler: OnModifiedEventHandler = OnModifiedEventHandler(tmp_path, {}, None)
+
+    file1: Path = tmp_path / "file.txt"
+    file2: Path = tmp_path / "file (1).txt"
+    file1.touch()
+    file2.touch()
+
+    incremented_path: Path = event_handler._increment_file_name(  # type: ignore[unused-ignore]
+        tmp_path,
+        file1,
+    )
+
+    assert incremented_path.name == "file (2).txt"
+
+
+def test_on_modified_event_handler_move_file(
+    tmp_path: Path,
+    extension_paths: dict[str, Path],
+) -> None:
+    path_for_undefined_extensions: Path = tmp_path / "undefined"
+    file_path: Path = tmp_path / "test_file.txt"
+    destination_path: Path = extension_paths[".txt"]
+
+    file_path.touch()
+
+    event_handler: OnModifiedEventHandler = OnModifiedEventHandler(
+        tmp_path,
+        extension_paths,
+        path_for_undefined_extensions=path_for_undefined_extensions,
+    )
+
+    event_handler._move_file(file_path)  # type: ignore[unused-ignore]
+
+    assert destination_path.exists()
+    assert (destination_path / f"{datetime.now():%Y/%b}" / "test_file.txt").exists()
+
+
+def test_on_modified_event_handler_on_modified_override() -> None:
+    assert OnModifiedEventHandler.on_modified.__override__  # type: ignore[unused-ignore, attr-defined] # pylint: disable=E1101
+
+
+@pytest.mark.skip(reason="Test needs to be written")  # type: ignore[misc]
+def test_on_modified_event_handler_on_modified() -> None:
+    ...
