d10207329ffbd4f86a2e4cf39ee6f1fecee5cb45
shouzy <82171453+realshouzy@users.noreply.github.com>
2023-08-05 00:01:29 +0200

    Removed setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Setup script, only to specify the url (Home-page)."""
-from __future__ import annotations
-
-from setuptools import setup
-
-setup(
-    url="https://github.com/realshouzy/auto-file-sorter",
-)
