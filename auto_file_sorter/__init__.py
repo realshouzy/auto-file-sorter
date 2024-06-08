931e9e47d5d8ce862d2239617785ef5f682cde1d
shouzy <82171453+realshouzy@users.noreply.github.com>
2024-06-08 20:25:26 +0200

    Bump version to 1.0.3

--- auto_file_sorter/__init__.py
+++ auto_file_sorter/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__: Final[str] = "1.0.2"
+__version__: Final[str] = "1.0.3"
 __title__: Final[str] = "auto-file-sorter"
 __author__: Final[str] = "realshouzy"
 __license__: Final[str] = "MIT"
